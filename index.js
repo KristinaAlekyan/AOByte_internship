1f56fd08253b8cd81c8860fe63d6577dc1c11dec
Kristina Alekyan <kristinaalekyan1@gmail.com>
2022-04-28 19:33:43 +0400

    homework 3

--- index.js
+++ index.js
@@ -1,141 +1,97 @@
-class MyCustomPromise {
-    constructor(handler) {
-        this.status = "pending";
-        this.onFulfilledCallbacks = [];
-        this.onRejectedCallbacks = [];
+class DomElement {
+  constructor(type, attr, children){
+  	this.type = type,
+  	this.attr = attr,
+  	this.children = children  
+  }
+  draw = () => {
+  	if(this.type === "div"){
+  		new DivElement(this.type, this.attr);
+  	} else if(this.type === "span"){
+  		new SpanElement(this.type, this.attr);
+  	} else if(this.type === "ul"){
+  		new UlElement(this.type, this.attr);
+  	} else if(this.type === "li"){
+  		new LiElement(this.type, this.attr);
+  	} else if(this.type === "form"){
+  		new FormElement(this.type, this.attr);
+  	} else if(this.type === "label"){
+  		new LabelElement(this.type, this.attr);
+  	} else if(this.type === "input"){
+  		new InputElement(this.type, this.attr);
+  	} else if(this.type === "br"){
+  		new BrElement(this.type, this.attr);
+  	}
+	  let element = document.createElement(this.type); 
 
-        const resolve = value => {
-            if (this.status === "pending") {
-                this.status = "fulfilled";
-                this.value = value;
-                this.onFulfilledCallbacks.forEach(fn => fn(value));
-            }
-        };
+		if(!Array.isArray(this.children) && typeof this.children ==="object" && this.children !== null){
+			element.appendChild(this.children.draw())
+		} else if(typeof this.children ==="string"){
+      element.appendChild(document.createTextNode(this.children))			
+		} else if(Array.isArray(this.children)){
+			this.children.forEach((child)=> element.appendChild(child.draw()))
+		}
 
-        const reject = value => {
-            if (this.status === "pending") {
-                this.status = "rejected";
-                this.value = value;
-                this.onRejectedCallbacks.forEach(fn => fn(value));
-            }
-        };
-
-        try {
-            handler(resolve, reject);
-        } catch (err) {
-            reject(err);
-        }
+    if(Object.keys(this.attr).length){
+      for(let [key,value] of Object.entries(this.attr) ){
+        element.setAttribute(key, value)
+      }
     }
-
-    then(onFulfilled, onRejected) {
-        return new MyCustomPromise((resolve, reject) => {
-            if (this.status === "pending") {
-                this.onFulfilledCallbacks.push(() => {
-                    try {
-                        const fulfilledFromLastPromise = onFulfilled(this.value);
-                        if (fulfilledFromLastPromise instanceof MyCustomPromise) {
-                            fulfilledFromLastPromise.then(resolve, reject);
-                        } else {
-                            resolve(fulfilledFromLastPromise);
-                        }
-                    } catch (err) {
-                        reject(err);
-                    }
-                });
-                this.onRejectedCallbacks.push(() => {
-                    try {
-                        const rejectedFromLastPromise = onRejected(this.value);
-                        if (rejectedFromLastPromise instanceof MyCustomPromise) {
-                            rejectedFromLastPromise.then(resolve, reject);
-                        } else {
-                            reject(rejectedFromLastPromise);
-                        }
-                    } catch (err) {
-                        reject(err);
-                    }
-                });
-            }
-
-            if (this.status === "fulfilled") {
-                try {
-                    const fulfilledFromLastPromise = onFulfilled(this.value);
-                    if (fulfilledFromLastPromise instanceof MyCustomPromise) {
-                        fulfilledFromLastPromise.then(resolve, reject);
-                    } else {
-                        resolve(fulfilledFromLastPromise);
-                    }
-                } catch (err) {
-                    reject(err);
-                }
-
-            }
-
-            if (this.status === "rejected") {
-                try {
-                    const rejectedFromLastPromise = onRejected(this.value);
-                    if (rejectedFromLastPromise instanceof MyCustomPromise) {
-                        rejectedFromLastPromise.then(resolve, reject);
-                    } else {
-                        reject(rejectedFromLastPromise);
-                    }
-                } catch (err) {
-                    reject(err);
-                }
-            }
-        });
-
-    }
+    return element
+	} 	
 }
 
-
-// let p = new MyCustomPromise((resolve, reject) => {
-//     setTimeout(() => resolve('resolved first one'), 5000);
-// });
-// p.then((res) => {
-//     console.log(res);
-//     return new MyCustomPromise(resolve => {
-//         setTimeout(() => resolve('resolved second one'), 1000);
-//     });
-// }).then(res => {
-//     console.log(res);
-// });
-
-
-
-function ajax(url, config) {    
-    return new MyCustomPromise((resolve, reject) => {     
-        let request = new XMLHttpRequest();   
-        request.open(config.type, url);
-        request.onload = function() {
-            if (request.status === 200) {
-                resolve(request.response)
-            } else {
-                reject('error')
-            }
-        }
-        request.onerror = function(e) {
-            reject(e)
-        }
-        request.send();
-    })
+class DivElement extends DomElement {
+  constructor(type, attr, children){
+    super(type, attr, children);    
+  } 
+}
+class SpanElement extends DomElement {
+  constructor(type, attr, children){
+    super(type, attr, children);    
+  } 
+}
+class UlElement extends DomElement {
+  constructor(type, attr, children){
+    super(type, attr, children);    
+  } 
+}
+class LiElement extends DomElement {
+  constructor(type, attr, children){
+    super(type, attr, children);    
+  } 
+}
+class FormElement extends DomElement {
+  constructor(type, attr, children){
+    super(type, attr, children);    
+  } 
+}
+class LabelElement extends DomElement {
+  constructor(type, attr, children){
+    super(type, attr, children);    
+  } 
+}
+class InputElement extends DomElement {
+  constructor(type, attr, children){
+    super(type, attr, children);    
+  } 
+}
+class BrElement extends DomElement {
+  constructor(type, attr, children){
+    super(type, attr, children);    
+  } 
 }
 
+const el = (type, attributes, children) => {
+  return new DomElement(type, attributes, children)
+}
 
-
-const p1 = ajax('https://dog.ceo/api/breeds/image/random', {
-    type: "GET",
-    headers: {},
-    data: {}
-}).then((res) => {
-        console.log(res);
-        return new MyCustomPromise(resolve => {
-            setTimeout(() => resolve('resolved first time'), 5000);
-    });
-}).then(res => {
-         console.log("res", res);
-});
-  
-
-  
-
-
+    
+const tree = el("div", {},
+              el("ul", {}, [
+                el("li", {}, "Item 1"),
+                el("li", {}, "Item 2"),
+                el("li", {}, "Item 3")
+              ])
+            );
+document.getElementById("root").appendChild(tree.draw());
